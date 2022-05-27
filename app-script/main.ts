03c1df1607ac106251c95df54efa450a50ba29c9
Miguel <30949939+donFaifo@users.noreply.github.com>
2022-05-27 15:06:04 +0200

    modficado título de la página principal

--- app-script/main.ts
+++ app-script/main.ts
@@ -1,7 +1,7 @@
 function doGet() {
     return HtmlService.createTemplateFromFile("index")
         .evaluate()
-        .setTitle("Test React y Typescript")
+        .setTitle("Cálculo fotovoltaica")
         .addMetaTag("viewport", "width=device-width, initial-scale=1.0, maximum-scale=1")
         .setFaviconUrl("https://mguerrero.me/imgpublic/ico-google-m.png");
 }
