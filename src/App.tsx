b057fedcfa3fee3e72480564e5f4bf250974c222
Miguel <30949939+donFaifo@users.noreply.github.com>
2022-06-01 07:27:29 +0200

    formulario de introducción de líneas en el presupuesto

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,33 @@ import Main from "./components/Main";
 import Container from "react-bootstrap/Container"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
+import { Col, Row } from "react-bootstrap";
 
 class App extends React.Component {
-    render() {
-        return <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Cálculos solar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Inicio</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
+  render() {
+    return <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Cálculos solar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-            <Main />
-            
-        </>;
-    }
+      <Main />
+
+      <Container>
+        <Row className="mt-5">
+          <Col className="d-flex justify-content-end">
+            Made by mGuerrero using React and Parcel for Google App Script
+          </Col>
+        </Row>
+      </Container>
+      
+
+    </>;
+  }
 }
 
 export default App
--- src/components/sections/EstimationComponents/EstimationForm.tsx
+++ src/components/sections/EstimationComponents/EstimationForm.tsx
@@ -1,5 +1,5 @@
 import React = require("react");
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 interface Props {
   isHidden: boolean
@@ -16,7 +16,53 @@ export default class EstimationForm extends React.Component<Props> {
 
     return <Row>
       <Col>
-        <p>Formulario</p>
+        <Form>
+          <Row>
+            <Col xs="12" md="4" xl="3" className="mb-4">
+              <Form.Label htmlFor="type">Tipo</Form.Label>
+              <Form.Select id="type">
+                <option value="panel">Panel solar</option>
+                <option value="battery">Batería</option>
+                <option value="controller">Controlador de carga</option>
+                <option value="inverter">Inversor</option>
+                <option value="structure">Estructura</option>
+                <option value="cable">Cableado</option>
+                <option value="other">Otros</option>
+              </Form.Select>
+            </Col>
+            <Col xs="12" md="8" xl="5" className="mb-4">
+              <Form.Label htmlFor="description">Descripción</Form.Label>
+              <Form.Control 
+                id="description"
+                type="text" 
+                placeholder="Descripción del producto"/>
+            </Col>
+            <Col className="mb-4" xs="12" md="4" xl="2">
+              <Form.Label htmlFor="ref">Referencia</Form.Label>
+              <Form.Control 
+                id="ref"
+                type="number" 
+                placeholder="Referencia LM"/>
+            </Col>
+            <Col className="mb-4" xs="12" md="2" xl="2">
+              <Form.Label htmlFor="qt">Cantidad</Form.Label>
+              <Form.Control 
+                id="qt"
+                type="number" 
+                placeholder="Cantidad"/>
+            </Col>
+            <Col className="mb-4" xs="12" md="3" xl="3">
+              <Form.Label htmlFor="price">Precio unitario</Form.Label>
+              <Form.Control 
+                id="price"
+                type="number" 
+                placeholder="Precio por unidad"/>
+            </Col>
+            <Col className="d-flex align-items-end mb-4" xs="2">
+              <Button>Guardar</Button>
+            </Col>
+          </Row>
+        </Form>
       </Col>
     </Row>
   }
