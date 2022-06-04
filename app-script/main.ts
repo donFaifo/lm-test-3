2de15e32e8f0bc6fc61b7a6db3aef7e3e4c1edec
Miguel <30949939+donFaifo@users.noreply.github.com>
2022-06-04 07:38:35 +0200

    modificaciones en el formato del documento de salida del presupuesto

--- app-script/main.ts
+++ app-script/main.ts
@@ -19,37 +19,73 @@ function doGet() {
 function createEstimate(data: Data) {
   folderId = getWorkingFolder()
   const date = new Date()
-  const shortDate = `${date.getDate().toString().padStart(1)}/${(date.getMonth()+1).toString().padStart(1)}/${date.getFullYear().toString()}`
+  const shortDate = `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth()+1).toString().padStart(2, "0")}/${date.getFullYear().toString()}`
   const fileName = data.name + " " + shortDate
   let document = DocumentApp.create(fileName)
   let folder = DriveApp.getFolderById(folderId)
   folder.addFile(DriveApp.getFileById(document.getId()))
 
+  let headingStyle = {}
+
+  headingStyle[DocumentApp.Attribute.FONT_FAMILY] = "Leroy Merlin Sans"
+  headingStyle[DocumentApp.Attribute.BOLD] = true
+
+  let rowHeadingStyle = {}
+
+  rowHeadingStyle[DocumentApp.Attribute.FONT_FAMILY] = "Leroy Merlin Sans"
+  rowHeadingStyle[DocumentApp.Attribute.BOLD] = true
+  rowHeadingStyle[DocumentApp.Attribute.FONT_SIZE] = 10
+
+  let rowStyle = {}
+
+  rowStyle[DocumentApp.Attribute.FONT_FAMILY] = "Leroy Merlin Sans"
+  rowStyle[DocumentApp.Attribute.BOLD] = false
+  rowStyle[DocumentApp.Attribute.FONT_SIZE] = 10
+
+  let normalStyle = {}
+
+  normalStyle[DocumentApp.Attribute.FONT_FAMILY] = "Leroy Merlin Sans"
+  normalStyle[DocumentApp.Attribute.BOLD] = false
+  normalStyle[DocumentApp.Attribute.FONT_SIZE] = 11
+
+
   document.getBody().insertParagraph(0, fileName)
     .setHeading(DocumentApp.ParagraphHeading.HEADING1)
+    .setAttributes(headingStyle)
 
   let table = document.getBody().appendTable()
   let header = table.appendTableRow()
-  header.appendTableCell("Referencia")
+  header.appendTableCell("Ref.")
   header.appendTableCell("Descripción")
-  header.appendTableCell("Cantidad")
-  header.appendTableCell("Precio unitario")
+  header.appendTableCell("Cant.")
+  header.appendTableCell("Precio u.")
   header.appendTableCell("Subtotal")
+  header.setAttributes(headingStyle)
 
   let productsList = data.list
+  let total = 0
 
   for(let i=0; i<productsList.length; i++) {
     let row = table.appendTableRow()
     productsList[i].product.forEach((product) => {
-      row.appendTableCell(product.ref.toString())
-      row.appendTableCell(product.description)
-      row.appendTableCell(product.qt.toString())
-      row.appendTableCell(product.price.toString() + " €")
-      row.appendTableCell((product.price * product.qt).toString() + " €")
+      let subtotal = product.price * product.qt
+      total += subtotal
+
+      row.appendTableCell(product.ref.toString()).setWidth(66)
+      row.appendTableCell(product.description).setWidth(200)
+      row.appendTableCell(product.qt.toString()).setWidth(54)
+      row.appendTableCell(product.price.toString() + " €").setWidth(66)
+      row.appendTableCell(subtotal.toString() + " €")
+      row.setAttributes(rowStyle)
     })
   }
 
+  document.getBody().appendParagraph("TOTAL: " + total.toString())
+    .setHeading(DocumentApp.ParagraphHeading.HEADING2)
+    .setAttributes(headingStyle)
+
   document.getBody().appendParagraph("Made with AppScript")
+    .setAttributes(normalStyle)
   
 }
 
